6975cd4dfbde3f1ea21f3db561fe02fb7bb7f9c4
Nut Bandera González <nutbanderag@gmail.com>
2021-04-25 17:33:58 +0200

    examinar enunciado

--- Scripts/CreateExercise.cs
+++ Scripts/CreateExercise.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneTemplate;
+using SFB;
 
 public class CreateExercise : MonoBehaviour
 {
-   [SerializeField] private InputField iField;
-
+    [SerializeField] private Text selectedEnunciado;
     public void createExercise() {
-        Interface.setEnunciado(iField.text);
         SceneManager.LoadScene("Scenes/ExerciseType1"); // depende del tipo de ejericio
+    }
+
+    public void selectFile() {
+        var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false);
+        // Comprobar que es una ruta válida
+        // if selected 
+        // let choose one only
+        var pathSplitted = paths[0].Split('/');
+        var elementName = pathSplitted[pathSplitted.Length - 1].ToString();
+        Interface.setEnunciado("Enunciados/" + elementName.Split('.')[0]); // removes extension
+        // show file selected
+        selectedEnunciado.text = elementName;
     }
 }
