a0a885224b779c16f89ad6a802183c27be7e4609
Nut Bandera González <nutbanderag@gmail.com>
2021-06-19 21:11:24 +0200

    delete items selected

--- Scripts/CreateExercise.cs
+++ Scripts/CreateExercise.cs
@@ -13,6 +13,13 @@ public class CreateExercise : MonoBehaviour
     [SerializeField] private Text selectedItems;
     [SerializeField] private Button contenidoButton;
     [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject panelItems;
+    private int xItem = 400;
+    private int xCross = 730;
+    private float y;
+    private List<GameObject> items = new List<GameObject>();
+    private List<GameObject> buttons = new List<GameObject>();
+    
     public void createExercise() {
         SceneManager.LoadScene("Scenes/ExerciseType1"); // depende del tipo de ejericio
     }
@@ -38,22 +45,35 @@ public class CreateExercise : MonoBehaviour
         var pathSplitted = path[0].Split('/');
         var elementName = pathSplitted[pathSplitted.Length - 1].ToString().Split('.')[0];
 
-        if (!CurrentExercise.getItems().Contains(elementName)) {
+        if (!CurrentExercise.getItems().Equals(elementName)) {
             CurrentExercise.addItem(elementName);
+            if (items.Count > 0){
+                y = items[items.Count-1].transform.position.y - 100;
+            } else {
+                y = 600;
+            }
+            // Crear item
+            GameObject item = new GameObject();
+            item.AddComponent<Image>();
+            item.GetComponent<Image>().sprite = Resources.Load<Sprite>("Items/"+elementName);
+            item.transform.position = new Vector3(xItem, y, 0f);
+            item.transform.parent = panelItems.transform;
+            item.GetComponent<RectTransform>().sizeDelta = new Vector2(80, 80); 
+
             // Create delete button and associate it with the element
             GameObject button = new GameObject();
-            button.transform.parent = panel.transform;
+            button.transform.parent = panelItems.transform;
             button.AddComponent<RectTransform>();
-            button.GetComponent<RectTransform>().sizeDelta = new Vector2(50, 50); 
+            button.GetComponent<RectTransform>().sizeDelta = new Vector2(80, 80); 
             button.AddComponent<Button>();
-            button.transform.position = new Vector3(200, 200, 0f); 
+            button.transform.position = new Vector3(xCross, y, 0f); 
             button.AddComponent<Image>();
             button.GetComponent<Image>().sprite = Resources.Load<Sprite>("cross");
-	        button.GetComponent<Button>().onClick.AddListener(delegate{deleteItem(elementName);});
-            // crear cuadro de texto y botón -> al clickar destruir los dos (dentro de un mismo panel?)
-            // auto destruir
-             // show items selected
-            selectedItems.text += " " + elementName;
+	        button.GetComponent<Button>().onClick.AddListener(delegate{deleteItem(elementName, item, button);});
+
+            items.Add(item);
+            buttons.Add(button);
+
         } else {
             // show message
             Debug.Log("Ya has seleccionado ese elemento");
@@ -64,10 +84,27 @@ public class CreateExercise : MonoBehaviour
         } 
     }
 
-    private void deleteItem(string elementName) {
-        // delete from list
-        Debug.Log("works");
+    private void deleteItem(string elementName, GameObject item, GameObject button) {
+        // Delete from list
         CurrentExercise.removeItem(elementName);
+        // Delete from scroll
+        Destroy(button.gameObject);
+        Destroy(item.gameObject);
+        // reassign positions
+        int index = items.IndexOf(item);
+        int i;
+        for (i=index+1; i<items.Count; i++){
+            // add 100 to each
+            items[i].transform.position = new Vector3(items[i].transform.position.x,
+            items[i].transform.position.y + 100, 0f);
+        }
+        for (i=index+1; i<buttons.Count; i++){
+            // add 100 to each
+            buttons[i].transform.position = new Vector3(buttons[i].transform.position.x,
+            buttons[i].transform.position.y + 100, 0f);
+        }
+        items.Remove(item);
+        buttons.Remove(button);
     }
 
     public void crearContenido() {
